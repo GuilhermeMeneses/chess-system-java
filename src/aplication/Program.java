be16477fe9d0929e33f694eff1cc6205ff6ce6d1
Guilherme <guimxac14@gmail.com>
2024-08-18 14:37:41 -0300

    Starting to implement Border and Piece

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,13 @@
 package aplication;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world");
-		Position pos = new Position(3, 5);
-		System.out.println(pos);
+		Board board =new Board(8,8);
+		
 	}
 
 }
