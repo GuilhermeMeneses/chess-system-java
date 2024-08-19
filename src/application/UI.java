355b041268c8ce7c9ab67fd73ff88da8b5c88d3d
Guilherme <guimxac14@gmail.com>
2024-08-19 14:40:05 -0300

    possible moves of a piece

--- src/application/UI.java
+++ src/application/UI.java
@@ -32,21 +32,20 @@ public class UI {
 
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
 	public static void clearScreen() {
-	 System.out.print("\033[H\033[2J");
-	 System.out.flush();
-	} 
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessPosition(Scanner sc) {
 		try {
-		String s = sc.nextLine();
-		char column = s.charAt(0);
-		int row = Integer.parseInt(s.substring(1));
-		return new ChessPosition(column, row);
-		}
-		catch(RuntimeException e) {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		} catch (RuntimeException e) {
 			throw new InputMismatchException("Error,Valid values a1-h8");
 		}
 	}
-
 
 	public static void printBoard(ChessPiece[][] pieces) {
 		for (int i = 0; i < pieces.length; i++) {
